5fff1910bc2b18bae5134cdd53827ff7c82c7a3c
FlorianWenzel <github@florian-wenzel.io>
2020-04-24 19:23:56 +0200

    add pwa splashscreen

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+// Inside vue.config.js
+module.exports = {
+  // ...other vue-cli plugin options...
+  pwa: {
+    name: 'Familie Wenzel',
+    themeColor: '#2A7118',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black-translucent',
+  }
+}

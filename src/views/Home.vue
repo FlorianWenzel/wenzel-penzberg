94f24d964851a8bad46b5aaf5bfba746eeaff5e5
FlorianWenzel <github@florian-wenzel.io>
2020-04-25 10:59:12 +0200

    add pull to refresh to home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="home">
-        <div>
-            <Zoom :mobile="mobile" :images="allImages" :i="openIndex" @close="openIndex = -1"></Zoom>
+        <Zoom :mobile="mobile" :images="allImages" :i="openIndex" @close="openIndex = -1"></Zoom>
+        <div id="posts">
             <Post @editPost="editPost" @imageClick="imageClick" :mobile="mobile" :user="user" :post="posts[index - 1]" :key="index" v-for="index in amountToDisplay"></Post>
         </div>
         <infinite-loading @infinite="infiniteHandler">
@@ -16,6 +16,7 @@ import * as env from '../assets/env';
 import Post from "../components/Post";
 import InfiniteLoading from 'vue-infinite-loading';
 import Zoom from "../components/Zoom";
+import PullToRefresh from 'pulltorefreshjs';
 
 export default {
     name: 'Home',
@@ -27,21 +28,15 @@ export default {
             amountToDisplay: 0,
             allImages: [],
             openIndex: -1,
+            infiniteState: null,
         }
     },
     methods: {
         async infiniteHandler($state){
+            if($state && !this.infiniteState)
+                this.infiniteState = $state;
             if(this.posts.length === 0){
-                this.posts = await axios.get(env.backend_url + '/posts?token=' + localStorage.getItem('token'))
-                    .then(({data}) => {
-                        for(const post of data){
-                            for(const image of post.images){
-                                image.thumbnail = image.thumbnail_url;
-                            }
-                            this.allImages.push(...post.images);
-                        }
-                        return data;
-                    });
+                await this.refresh();
                 if(this.posts.length === 0){
                     $state.complete();
                     return;
@@ -55,13 +50,40 @@ export default {
                 this.amountToDisplay += 5;
                 $state.loaded();
             }
-      },
+        },
+        refresh(){
+            this.posts = [];
+            this.amountToDisplay = 0;
+            return new Promise((resolve) => {
+                axios.get(env.backend_url + '/posts?token=' + localStorage.getItem('token'))
+                .then(({data}) => {
+                    for(const post of data){
+                        this.allImages.push(...post.images);
+                    }
+                    this.posts = data;
+                    this.infiniteHandler(this.infiniteState);
+                    resolve();
+                });
+            })
+        },
         editPost(post){
             this.$emit('editPost', post);
         },
         imageClick(image){
-          this.openIndex = this.allImages.indexOf(image);
+            this.openIndex = this.allImages.indexOf(image);
         }
+    },
+    mounted(){
+        const _this = this;
+        PullToRefresh.init({
+            mainElement: '#posts',
+            instructionsPullToRefresh: 'Aktualisieren?',
+            instructionsReleaseToRefresh: 'Loslassen, um zu aktuallisieren',
+            instructionsRefreshing: 'Aktuallisiert!',
+            onRefresh() {
+                _this.refresh();
+            }
+        });
     }
 }
 </script>

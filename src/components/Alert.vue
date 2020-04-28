60df4274d25a2ac0df10ebf3bc395ef8243c1613
FlorianWenzel <github@florian-wenzel.io>
2020-04-28 16:53:27 +0200

    add alert component

---
+++ src/components/Alert.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="`alert alert-${type} alert-dismissible fade ${show ? 'show' : ''}`" role="alert">
+    <div v-html="html">
+
+    </div>
+    <button type="button" class="close" data-dismiss="alert" @click="$emit('close')" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Alert.vue",
+    props: ["show", "type", "html"],
+
+  }
+</script>
+
+<style scoped>
+.alert {
+  position: fixed;
+  width: 98vw;
+  left: 1vw;
+  bottom: 1vw;
+  z-index: 103;
+  margin: 0;
+}
+</style>
